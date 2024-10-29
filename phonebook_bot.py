548b41403aae85a8d86ee60bb0de4baebc350e57
anastasiia-kushch <anastasiia.kushch02@gmail.com>
2024-10-29 14:09:29 +0100

    finish

--- phonebook_bot.py
+++ phonebook_bot.py
@@ -1,3 +1,34 @@
+"""
+    Assistant bot for contact management and birthday tracking.
+
+    This bot facilitates easy contact and birthday management with various commands, providing colored outputs for user feedback.
+
+    Main functionalities:
+    - 'add <name> <phone>': adds a new contact with the specified name and phone number, or updates an existing contact by adding a new phone number.
+    - 'change <name> <old phone> <new phone>': changes the specified phone number of an existing contact.
+    - 'phone <name>': displays all phone numbers associated with the specified contact.
+    - 'all': lists all contacts in the address book with their details.
+    - 'add-birthday <name> <birthday>': adds a birthday to a contact (format: 'DD.MM.YYYY').
+    - 'show-birthday <name>': shows the birthday for a specific contact.
+    - 'birthdays': displays contacts with upcoming birthdays within the next week.
+    - 'hello': responds with a greeting message.
+    - 'info': shows a list of available commands and their descriptions.
+    - 'close' or 'exit': exit the bot.
+
+    Error Handling:
+    - the bot gracefully handles common errors such as missing arguments, invalid inputs, and non-existent contacts using the `input_error` decorator.
+    - errors including KeyError, ValueError, and IndexError are managed to prevent application crashes and return user-friendly messages.
+    - unexpected errors are intercepted and reported with a general message, ensuring continuous bot operation.
+
+    Returns:
+    None: the bot outputs results directly to the console, including feedback on command execution and error messages.
+
+    Additional Notes:
+    - the bot uses the `colorama` library to apply color-coded output for clearer user interactions, differentiating success messages, errors, and informational feedback.
+    - the bot operates in a loop, processing user commands until the user inputs 'close' or 'exit'.
+"""
+
+
 from collections import UserDict
 from datetime import datetime
 import re
@@ -43,12 +74,12 @@ class Birthday(Field):
 
 class Record:
     def __init__(self, name):
-        self.name = Name(name)
+        self.name = name
         self.phones = []
         self.birthday = None
 
     def __str__(self):
-        return f"contact name: {self.name}, phones: {'; '.join(str(p) for p in self.phones)}"
+        return f"contact name: {self.name}, phones: {'; '.join(str(p) for p in self.phones)}, birthday: {self.birthday}"
 
     def add_phone(self, phone: Phone):
             self.phones.append(phone)
@@ -74,7 +105,7 @@ class Record:
 
     def add_birthday(self, birthday: Birthday):
         self.birthday = birthday
-        return (f"Birthday {birthday} added")
+        return (f"{self.name}'s birthday on {birthday} added")
 
 
        
@@ -98,26 +129,33 @@ class AddressBook(UserDict):
     def get_upcoming_birthdays(self):
         congrats_list = []
         today = datetime.now().date()
-    
+
         for contact in self.data.values():
-            birthday = contact.birthday
-            birthday_this_year = birthday.replace(year=today.year)
+            if contact.birthday:
+                birthday = datetime.strptime(contact.birthday, "%d.%m.%Y").date()
+                birthday_this_year = birthday.replace(year=today.year)
 
-            if birthday_this_year < today:
-                birthday_this_year = birthday.replace(year=today.year + 1)
+                if birthday_this_year < today:
+                    birthday_this_year = birthday.replace(year=today.year + 1)
             
-            days_until_birthday = (birthday_this_year - today).days
+                days_until_birthday = (birthday_this_year - today).days
 
-            if 0 <= days_until_birthday <= 7:
+                if 0 <= days_until_birthday <= 7:
 
-                if birthday_this_year.weekday() == 5:
-                    birthday_this_year += timedelta(days=2)
-                elif birthday_this_year.weekday() == 6:
-                    birthday_this_year += timedelta(days=1)
+                    if birthday_this_year.weekday() == 5:
+                        birthday_this_year += timedelta(days=2)
+                    elif birthday_this_year.weekday() == 6:
+                        birthday_this_year += timedelta(days=1)
 
-                congrats_list.append({ contact.name: birthday_this_year.strftime("%d.%m.%Y") })
+                    congrats_list.append({ contact.name: birthday_this_year.strftime("%d.%m.%Y") })
+            
 
-        return congrats_list
+        if len(congrats_list) > 0:
+            return congrats_list
+        else:
+            return None
+
+        
 
 
 
@@ -235,18 +273,25 @@ def show_all_birthdays(book: AddressBook):
         raise ValueError(colored_error("No contacts available."))
     
     result = book.get_upcoming_birthdays()
-    return colored_output('\n'.join(result))
+
+    if not result:
+        return colored_output("No birthdays soon.")
+    
+    formatted_result = [f'{name}: {date}' for birthday_dict in result for name, date in birthday_dict.items()]
+    return colored_output('\n'.join(formatted_result))
     
     
 
-
-# ?? @input_error
 def show_info():
     result = [
-        colored_output('add <name> <phone>') + ': adds a new contact (e.g., add John 123456789)',
-        colored_output('change <name> <phone>') + ': changes the phone number of an existing contact (e.g., change John 987654321)',
-        colored_output('phone <name>') + ': shows the phone number of the specified contact (e.g., phone John)',
-        colored_output('all') + ': shows all contacts in your phonebook',
+        colored_output('add <name> <phone>') + ': adds a new contact with the name and phone number, or adds a phone number to an existing contact (e.g., add John 123456789)',
+        colored_output('change <name> <old phone> <new phone>') + ': changes the phone number of an existing contact (e.g., change John 123456789 987654321)',
+        colored_output('phone <name>') + ': shows the phone number(s) of the specified contact (e.g., phone John)',
+        colored_output('all') + ': shows all contacts in your address book',
+        colored_output('add-birthday <name> <birthday>') + ': adds a birthday for the specified contact (e.g., add-birthday John 01.01.1990)',
+        colored_output('show-birthday <name>') + ': shows the birthday of the specified contact (e.g., show-birthday John)',
+        colored_output('birthdays') + ': shows upcoming birthdays within the next week',
+        colored_output('hello') + ': receive a greeting from the bot',
         colored_output('info') + ': displays the list of available commands',
         colored_output('close or exit') + ': exits the application'
     ]
@@ -267,31 +312,34 @@ def main():
             break
 
         elif command == "hello":
-            print(colored_info("How can I help you?" + Fore.RESET)) #works
+            print(colored_info("How can I help you?" + Fore.RESET)) 
+
+        elif command == "info":
+            print(show_info()) 
 
         elif command == "add":
-            print(add_contact(args, book)) #works
+            print(add_contact(args, book)) 
 
         elif command == "change":
-            print(change_contact(args, book)) #works
+            print(change_contact(args, book)) 
 
         elif command == "phone":
-            print(show_phone(args, book)) #works
+            print(show_phone(args, book)) 
 
         elif command == "all":
-            print(show_all(book)) #works
+            print(show_all(book)) 
 
         elif command == "add-birthday":
-            print(add_birthday(args, book)) #works
+            print(add_birthday(args, book))
 
         elif command == "show-birthday":
-            print(show_birthday(args, book)) #works
+            print(show_birthday(args, book)) 
 
         elif command == "birthdays":
             print(show_all_birthdays(book))
 
         else:
-            print("Invalid command.")
+            print(colored_error("Invalid command."))
 
 if __name__ == "__main__":
     main()
